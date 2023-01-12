90a14c07f8bf1812d8f1f359de47235b26a84a47
Elfranne <tom.braarup@gmail.com>
2023-01-12 15:43:58 +0100

    testing range

--- types/options.pp
+++ types/options.pp
@@ -9,4 +9,5 @@ type IPSet::Options = Struct[{
     Optional[maxelem]  => Integer[128],
     Optional[netmask]  => IP::Address,
     Optional[timeout]  => Integer[1],
+    Optional[range]    => String,
 }]
